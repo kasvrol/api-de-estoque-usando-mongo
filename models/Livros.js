aa4bbb2d3379db25be62a1396e487f46519f4db1
ana.karolina <ana.karolina@ablsystem.com.br>
2022-05-26 12:04:31 -0300

    feat: diretorio que abrigará o modelo de livros

---
+++ models/Livros.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+const livroSchema = new mongoose.Schema({
+    id: { type: String },
+    titulo: { type: String, required: true },
+    autor: { type: String, required: true },
+    editora: { type: String, required: true },
+    numeroPaginas: { type: Number },
+})
+
+exports.modules = mongoose.model('livros', livroSchema)
