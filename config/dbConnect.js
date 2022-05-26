b8112021e443893558181241e8df36fc3bfc7507
ana.karolina <ana.karolina@ablsystem.com.br>
2022-05-26 11:56:58 -0300

    fix: mensage de sucesso com o Mongo

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -4,7 +4,7 @@ const db = mongoose.connect('mongodb://localhost/tresawallethomolog', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => {
-    console.log('Conexão com mongo sucesso')
-}).catch((error) => { console.log(error) })
+    console.log('Conexão com mongo realizada com sucesso')
+}).catch(() => { console.log('Perda da conexão com o Mongo') })
 
 exports.modules = db
